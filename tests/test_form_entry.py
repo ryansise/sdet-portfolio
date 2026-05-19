dda63dc430304664c8c9c8fc06528e0f43182904
Ryan Sise <ryan@rdsise.tech>
2026-05-19 13:50:35 -0400

    added assertions for valid fields in email test

--- tests/test_form_entry.py
+++ tests/test_form_entry.py
@@ -71,6 +71,10 @@ def test_invalid_email(playwright: Playwright):
     page.locator("input[type='radio'][value='Male']").check()
     page.click("#submit")
     expect(page.locator("#userEmail:invalid")).to_have_count(1)
+    expect(page.locator("#gender-radio-1:invalid")).to_have_count(0)
+    expect(page.locator("#lastName:invalid")).to_have_count(0)
+    expect(page.locator("#firstName:invalid")).to_have_count(0)
+    expect(page.locator("#userNumber:invalid")).to_have_count(0)
     browser.close()
 
 def test_invalid_phone(playwright: Playwright):
