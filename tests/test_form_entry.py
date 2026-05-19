38a55939688c035fa00f12dbfab81ed262f9e727
Ryan Sise <ryan@rdsise.tech>
2026-05-19 13:47:05 -0400

    added assertions for valid fields in lastName test

--- tests/test_form_entry.py
+++ tests/test_form_entry.py
@@ -35,6 +35,10 @@ def test_invalid_lastName(playwright: Playwright):
     page.locator("input[type='radio'][value='Male']").check()
     page.click("#submit")
     expect(page.locator("#lastName:invalid")).to_have_count(1)
+    expect(page.locator("#firstName:invalid")).to_have_count(0)
+    expect(page.locator("#userEmail:invalid")).to_have_count(0)
+    expect(page.locator("#gender-radio-1:invalid")).to_have_count(0)
+    expect(page.locator("#userNumber:invalid")).to_have_count(0)
     browser.close()
 
 def test_invalid_gender(playwright: Playwright):
