abc6134fd4841203d9c12ff7eea3fd3f330466aa
Ryan Sise <ryan@rdsise.tech>
2026-05-19 12:55:29 -0400

    removed fields with no data validations, added passing test for invalid last name

--- tests/test_form_entry.py
+++ tests/test_form_entry.py
@@ -13,13 +13,25 @@ def test_invalid_firstName(playwright: Playwright):
     page.fill("#lastName","Smith")
     page.fill("#userEmail","joe@somewhere.com")
     page.get_by_role("radio").filter(has_text="Male").click
-    page.fill("#userNumber","123-456-7890")
-    page.fill("#dateOfBirthInput","17 Nov 1980")
-    page.fill("#subjectsInput","Linux")
-    page.fill("#currentAddress","123 Main St")
+    page.fill("#userNumber","1234567890")
     page.click("#submit")
     expect(page.locator("#firstName:invalid")).to_have_count(1)
     browser.close()
 
+def test_invalid_lastName(playwright: Playwright):
+    chromium = playwright.chromium
+    browser = chromium.launch()
+    page = browser.new_page()
+    page.goto("https://demoqa.com/automation-practice-form")
+    nameField = page.locator("#lastName")
+    nameField.fill("")
+    page.fill("#firstName", "Joe")
+    page.fill("#userEmail","joe@somewhere.com")
+    page.get_by_role("radio").filter(has_text="Male").click
+    page.click("#submit")
+    expect(page.locator("#lastName:invalid")).to_have_count(1)
+    browser.close()
+
 with sync_playwright() as playwright:
     test_invalid_firstName(playwright)
+    test_invalid_lastName(playwright)
