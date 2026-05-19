a533c210eb612e76a2b17d0c721307377df56446
Ryan Sise <ryan@rdsise.tech>
2026-05-19 12:44:37 -0400

    resolved invalid assertions & test passes, updated function name

--- tests/test_form_entry.py
+++ tests/test_form_entry.py
@@ -3,13 +3,13 @@
 import pytest
 from playwright.sync_api import sync_playwright, Playwright, expect
 
-def test_kyc_ui(playwright: Playwright):
+def test_invalid_firstName(playwright: Playwright):
     chromium = playwright.chromium
     browser = chromium.launch()
     page = browser.new_page()
     page.goto("https://demoqa.com/automation-practice-form")
-    #nameField = page.fill("#firstName","")
-    #errorIcon = nameField.locator(".form-control:invalid")
+    nameField = page.locator("#firstName")
+    nameField.fill("")
     page.fill("#lastName","Smith")
     page.fill("#userEmail","joe@somewhere.com")
     page.get_by_role("radio").filter(has_text="Male").click
@@ -18,8 +18,8 @@ def test_kyc_ui(playwright: Playwright):
     page.fill("#subjectsInput","Linux")
     page.fill("#currentAddress","123 Main St")
     page.click("#submit")
-    expect(errorIcon).to_be_visible()
+    expect(page.locator("#firstName:invalid")).to_have_count(1)
     browser.close()
 
 with sync_playwright() as playwright:
-    test_kyc_ui(playwright)
+    test_invalid_firstName(playwright)
