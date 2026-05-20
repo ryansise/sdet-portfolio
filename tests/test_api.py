813f7f61eb2e52eed80767f392b1bcdd430ddc8a
Ryan Sise <ryan@rdsise.tech>
2026-05-20 13:45:20 -0400

    added negative assertion, created test for post method

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,7 @@ import pytest, requests
 
 def test_api():
     baseURL = "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(baseURL)
+    response = requests.get(baseURL, timeout=30)
     assert response.status_code == 200
 
     data = response.json()
@@ -13,3 +13,19 @@ def test_api():
     assert "title" in data[0]
     assert "body" in data[0]
     assert "userId" in data[0]
+    assert "name" not in data[0]
+
+def add_post():
+    baseURL = "https://jsonplaceholder.typicode.com/posts"
+    payload = {
+        "userId": 5,
+        "id": 15,
+        "title": "This is not the post you're looking for",
+        "body": "You want to go home & re-think your life"
+    }
+    response = requests.post(baseURL,json=payload)
+    assert response.status_code == 201
+
+    json_response = response.json()
+    assert json_response["userId"] == 5
+    assert json_response["id"] == 15
