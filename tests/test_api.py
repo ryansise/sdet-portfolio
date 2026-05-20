d162b9f8aa75cf6ee78d7eef83f640f4dbc99260
Ryan Sise <ryan@rdsise.tech>
2026-05-20 13:31:18 -0400

    added assertions to verify data content

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,3 +4,12 @@ def test_api():
     baseURL = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(baseURL)
     assert response.status_code == 200
+
+    data = response.json()
+    assert len(data) > 0
+    assert isinstance(data,list)
+    assert isinstance(data[0],dict)
+    assert "id" in data[0]
+    assert "title" in data[0]
+    assert "body" in data[0]
+    assert "userId" in data[0]
