4d95cccb98103d2bdcbe0bd9206042134138e1bc
Ryan Sise <ryan@rdsise.tech>
2026-05-20 13:56:23 -0400

    removed useless assertion, updated test names, assertions for post test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import pytest, requests
 
-def test_api():
+def test_api_get_functionality():
     baseURL = "https://jsonplaceholder.typicode.com/posts"
     response = requests.get(baseURL, timeout=30)
     assert response.status_code == 200
@@ -13,19 +13,19 @@ def test_api():
     assert "title" in data[0]
     assert "body" in data[0]
     assert "userId" in data[0]
-    assert "name" not in data[0]
 
-def add_post():
+def test_api_post_functionality():
     baseURL = "https://jsonplaceholder.typicode.com/posts"
     payload = {
         "userId": 5,
-        "id": 15,
         "title": "This is not the post you're looking for",
         "body": "You want to go home & re-think your life"
     }
-    response = requests.post(baseURL,json=payload)
+
+    response = requests.post(baseURL,json=payload,timeout=30)
     assert response.status_code == 201
 
     json_response = response.json()
     assert json_response["userId"] == 5
-    assert json_response["id"] == 15
+    assert json_response["title"] == payload["title"]
+    assert json_response["body"] == payload["body"]
