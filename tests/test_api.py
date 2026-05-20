4f5c447ff6d5846b7d4909c0da006439ef20427d
Ryan Sise <ryan@rdsise.tech>
2026-05-20 13:16:53 -0400

    re-factored api tests to use requests library

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,6 @@
-import pytest
-from playwright.sync_api import sync_playwright, Playwright, expect
+import pytest, requests
 
-def test_api(playwright: Playwright):
-    context = playwright.request.new_context()
-    response = context.get("https://jsonplaceholder.typicode.com/posts")
-    expect(response).to_be_ok
-
-with sync_playwright() as playwright:
-    test_api(playwright)
+def test_api():
+    baseURL = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(baseURL)
+    assert response.status_code == 200
