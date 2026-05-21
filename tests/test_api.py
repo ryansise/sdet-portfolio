d9c02113265af0fc66a7f9ba6d35ea4ccdc00b70
Ryan Sise <ryan@rdsise.tech>
2026-05-21 16:11:22 -0400

    updated url variable name, added negative GET test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,8 @@
 import pytest, requests
 
 def test_api_get_functionality():
-    baseURL = "https://jsonplaceholder.typicode.com/posts"
-    response = requests.get(baseURL, timeout=30)
+    base_url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(base_url, timeout=30)
     assert response.status_code == 200
 
     data = response.json()
@@ -15,17 +15,24 @@ def test_api_get_functionality():
     assert "userId" in data[0]
 
 def test_api_post_functionality():
-    baseURL = "https://jsonplaceholder.typicode.com/posts"
+    base_url = "https://jsonplaceholder.typicode.com/posts"
     payload = {
         "userId": 5,
         "title": "This is not the post you're looking for",
         "body": "You want to go home & re-think your life"
     }
 
-    response = requests.post(baseURL,json=payload,timeout=30)
+    response = requests.post(base_url,json=payload,timeout=30)
     assert response.status_code == 201
 
     json_response = response.json()
     assert json_response["userId"] == 5
     assert json_response["title"] == payload["title"]
     assert json_response["body"] == payload["body"]
+    assert "id" in json_response
+
+def test_api_get_negative():
+    base_url = "https://jsonplaceholder.typicode.com/posts/999999999"
+    response = requests.get(base_url, timeout=30)
+
+    assert response.status_code == 404
