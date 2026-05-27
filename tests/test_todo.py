8d18f67e07945fae7ee05f626432234847aad8f5
Ryan Sise <ryan@rdsise.tech>
2026-05-27 13:55:24 -0400

    re-named fixture

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -2,15 +2,15 @@ import pytest
 from playwright.sync_api import expect
 
 @pytest.fixture
-def session(browser):
+def todo_page(browser):
     page = browser.new_page()
     page.goto("https://todomvc.com/examples/react/dist/")
     yield page
     page.close()
 
-def test_add_todo(session):
-    session.fill(".new-todo", "Test KYC Workflow")
-    session.press(".new-todo", "Enter")
-    todo_items = session.locator("ul > li")
+def test_add_todo(todo_page):
+    todo_page.fill(".new-todo", "Test KYC Workflow")
+    todo_page.press(".new-todo", "Enter")
+    todo_items = todo_page.locator("ul > li")
     expect(todo_items).to_contain_text(["Test KYC Workflow"])
 
